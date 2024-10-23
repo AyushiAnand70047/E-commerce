db98e35ea88af2409613f10487b8dbb80c71988a
AyushiAnand70047 <ayushianand088@gmail.com>
2024-10-23 14:46:08 +0530

    Project setup done

---
+++ eComm/resources/views/login.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+@extends('master')
+@section('content')
+<h1>This is login page</h1>
+<button class="btn btn-primary">Click on me</button>
+</body>
+</html>
